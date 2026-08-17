1fc8b069ffd8e3d6151adb810345a0a2c2e66fe7
quynhnngbs17084 <ngyn.qynh@gmail.com>
2026-08-17 19:52:16 +0700

    feat: add active state to sidebar navigation

--- app/components/sidebar/sidebar-menu/menu-item/index.tsx
+++ app/components/sidebar/sidebar-menu/menu-item/index.tsx
@@ -1,37 +1,58 @@
 import type { ReactNode } from "react";
 import styles from "./style.module.scss";
+import { NavLink } from "react-router";
 
 type MenuItemProps = {
   iconLeft: ReactNode;
   title: string;
-  isActive?: boolean;
   suffixIcon?: ReactNode;
   count?: number;
-  action: () => void;
-  onClick: () => void;
+  action?: () => void;
+  onClick?: () => void;
+  to?: string;
 };
 
 export const MenuItem = ({
   iconLeft,
   title,
-  isActive,
   suffixIcon,
   count,
   action,
-  onClick
+  onClick,
+  to,
 }: MenuItemProps) => {
+  const content = (
+    <div className={styles["item-wrapper"]}>
+      <div className={styles["item-content"]}>
+        {iconLeft && iconLeft}
+        <span>{title}</span>
+      </div>
+      {!!count && <span className={styles["item-count"]}>{count}</span>}
+    </div>
+  );
+
+  if (to) {
+    return (
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive ? `${styles["item"]} ${styles["active"]}` : styles["item"]
+        }
+        onClick={onClick}
+      >
+        {content}
+      </NavLink>
+    );
+  }
+
   return (
     <div
-      className={isActive ? `${styles["item"]} ${styles["active"]}` : styles["item"]} 
-      onClick={() => {action?.(), onClick?.()}}
+      className={styles["item"]}
+      onClick={() => {
+        (action?.(), onClick?.());
+      }}
     >
-      <div className={styles["item-wrapper"]}>
-        <div className={styles["item-content"]}>
-          {iconLeft}
-          <span>{title}</span>
-        </div>
-        {!!count && <span className={styles["item-count"]}>{count}</span>}
-      </div>
+      {content}
       {!!suffixIcon && (
         <button className={styles["item-suffix"]}>{suffixIcon}</button>
       )}
